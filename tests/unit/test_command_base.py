bf97bb30e013f1529622135d3d555e95d9fe5b81
StepWolf45 <s.andreichev@yandex.ru>
2025-09-26 12:16:35 +0300

    tests: unit test for abstraction

---
+++ tests/unit/test_command_base.py
@@ -0,0 +1,19 @@
+from src.server.commands.command_base import Command
+
+
+class Dummy(Command):
+    def execute(self, args):
+        return True, args
+
+    def get_name(self):
+        return "DUMMY"
+
+
+def test_validate_args_bounds():
+    assert Command.validate_args(["a"], 1, 2) is True
+    assert Command.validate_args(["a", "b"], 1, 2) is True
+    assert Command.validate_args([], 1, 2) is False
+    assert Command.validate_args(["a", "b", "c"], 1, 2) is False
+
+
+
